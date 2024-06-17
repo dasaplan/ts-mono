a473fa77db929d9ebfebddb511b09bdc8a168e45
dsp <dev.dsp@posteo.de>
2024-06-17 23:56:41 +0200

    feat: handle dangling properties for schema with allOf

--- packages/openapi-bundler/src/post-process/spec/merge-all-of.ts
+++ packages/openapi-bundler/src/post-process/spec/merge-all-of.ts
@@ -2,45 +2,71 @@
 
 import { OpenApiBundled } from "../../bundle.js";
 import { oas30 } from "openapi3-ts";
-import {_, ApplicationError} from "@dasaplan/ts-sdk";
+import { _, ApplicationError } from "@dasaplan/ts-sdk";
 
 import jsonSchemaMergeAllOff from "json-schema-merge-allof";
 import { isRef } from "@redocly/openapi-core";
 import { cleanObj, SchemaResolverContext } from "../../resolver/index.js";
+import { log } from "../../logger.js";
 
 export function mergeAllOf(bundled: OpenApiBundled) {
   const mergedAllOf = _.cloneDeep(bundled);
   const { collected, ctx } = findSchemaObjectsWithAllOf(mergedAllOf);
   collected.forEach((s) => {
-      try {
-          doMerge(s.schema, ctx);
-      }catch (e){
-          throw ApplicationError.create(`failed to merge allOf for schema.id ${s.id}`).chainUnknown(e);
-      }
+    try {
+      doMerge(s, ctx);
+    } catch (e) {
+      throw ApplicationError.create(
+        `failed to merge allOf for schema.id ${s.id}`
+      ).chainUnknown(e);
+    }
   });
   return mergedAllOf;
 }
 
-function doMerge(schema: any, ctx: SchemaResolverContext) {
-  const subSchemas: Array<oas30.ReferenceObject | oas30.SchemaObject> = schema.allOf ?? [];
-
-  const clonedSchemas = _.cloneDeep(subSchemas);
-  const _resolvedSchemas = resolveSubSchemas(clonedSchemas, ctx);
+function doMerge(
+  { schema, id }: { id: string; schema: any },
+  ctx: SchemaResolverContext
+) {
+  const subSchemas: Array<oas30.ReferenceObject | oas30.SchemaObject> =
+    schema.allOf ?? [];
   // include dangling properties {allOf: [], danglingA: {}, danglingB: [], danglingC: null, ... }
   const danglingProperties = { ..._.omit(schema, "allOf") };
   if (!_.isEmpty(danglingProperties)) {
+    log.warn(
+      `fixing dangling properties for schema.id ${id} - moving all properties into allOf array`
+    );
+    schema.allOf?.push(danglingProperties);
+    cleanObj(schema, ["allOf"]);
+  }
+  const clonedSchemas = _.cloneDeep(subSchemas);
+  const _resolvedSchemas = resolveSubSchemas(clonedSchemas, ctx);
+
+  if (!_.isEmpty(danglingProperties)) {
     _resolvedSchemas.push({ pointer: undefined, resolved: danglingProperties });
   }
-  const hasOneOfOrAnyOf = _resolvedSchemas.filter((f) => !_.isEmpty(f.resolved.oneOf) || !_.isEmpty(f.resolved.anyOf))?.length > 0;
+  const hasOneOfOrAnyOf =
+    _resolvedSchemas.filter(
+      (f) => !_.isEmpty(f.resolved.oneOf) || !_.isEmpty(f.resolved.anyOf)
+    )?.length > 0;
   if (hasOneOfOrAnyOf) {
-    throw `Error: anyOf or oneOf are yet not supported as an allOf subschema: ${JSON.stringify(schema)}`;
+    throw `Error: anyOf or oneOf are yet not supported as an allOf subschema: ${JSON.stringify(
+      schema
+    )}`;
   }
 
-  const parentsWithDiscriminator = _resolvedSchemas.filter((p) => _.isDefined(p.pointer) && _.isDefined(p.resolved.discriminator));
-  const resolvedSchemas = _resolvedSchemas.filter((p) => !(_.isDefined(p.pointer) && _.isDefined(p.resolved.discriminator)));
+  const parentsWithDiscriminator = _resolvedSchemas.filter(
+    (p) => _.isDefined(p.pointer) && _.isDefined(p.resolved.discriminator)
+  );
+  const resolvedSchemas = _resolvedSchemas.filter(
+    (p) => !(_.isDefined(p.pointer) && _.isDefined(p.resolved.discriminator))
+  );
 
   if (resolvedSchemas.length < 1) {
     // something is off: could be a schema with a single allOf or an allOf comprised of multiple discriminators...
+    log.warn(
+      `found allOf with single element in schema: ${JSON.stringify(schema)}`
+    );
     return schema;
   }
 
@@ -85,7 +111,10 @@ function doMerge(schema: any, ctx: SchemaResolverContext) {
 function resolveSubSchemas(
   subSchemas: Array<oas30.ReferenceObject | oas30.SchemaObject>,
   ctx: SchemaResolverContext
-): Array<{ pointer: string | undefined; resolved: oas30.SchemaObject & { $ref?: string } }> {
+): Array<{
+  pointer: string | undefined;
+  resolved: oas30.SchemaObject & { $ref?: string };
+}> {
   const subs = subSchemas.map((d) => {
     return resolveRefNode(d, ctx, { deleteRef: true });
   });
@@ -94,26 +123,42 @@ function resolveSubSchemas(
     return subs;
   }
   const flatten = resolveSubSchemas(allOfs, ctx);
-  return [...flatten, ...subs].map((s) => ({ pointer: s.pointer, resolved: _.omit(s.resolved, "allOf") }));
+  return [...flatten, ...subs].map((s) => ({
+    pointer: s.pointer,
+    resolved: _.omit(s.resolved, "allOf"),
+  }));
 }
 
-function getJsonSchemaMergeAllOff(subschemas: Array<{ pointer: string | undefined; resolved: oas30.SchemaObject }>) {
+function getJsonSchemaMergeAllOff(
+  subschemas: Array<{
+    pointer: string | undefined;
+    resolved: oas30.SchemaObject;
+  }>
+) {
   try {
-      return jsonSchemaMergeAllOff(
-          { allOf: subschemas.map((s) => s.resolved) },
-          {
-              resolvers: {
-                  // overwrite title by higher indexed schema: [ {title: a}, {title:b}] => {title:b}
-                  title: ([a, b]) => b ?? a!,
-              },
-          }
-      );
-  }catch (e: unknown){
-      throw ApplicationError.create("failed merging allOf sub schemas").chainUnknown(e);
+    return jsonSchemaMergeAllOff(
+      { allOf: subschemas.map((s) => s.resolved) },
+      {
+        resolvers: {
+          // overwrite title by higher indexed schema: [ {title: a}, {title:b}] => {title:b}
+          title: ([a, b]) => b ?? a!,
+        },
+      }
+    );
+  } catch (e: unknown) {
+    throw ApplicationError.create(
+      "failed merging allOf sub schemas"
+    ).chainUnknown(e);
   }
 }
 
-function mergeSubSchemas(_resolvedSchemas: Array<{ pointer: string | undefined; resolved: oas30.SchemaObject }>, ctx: SchemaResolverContext) {
+function mergeSubSchemas(
+  _resolvedSchemas: Array<{
+    pointer: string | undefined;
+    resolved: oas30.SchemaObject;
+  }>,
+  ctx: SchemaResolverContext
+) {
   const subschemas = _resolvedSchemas.map((d) => ({
     pointer: d.pointer,
     resolved: ctx.resolver.resolveRef(d.resolved, { deleteRef: true }),
@@ -127,7 +172,10 @@ function mergeSubSchemas(_resolvedSchemas: Array<{ pointer: string | undefined; 
       return;
     }
     // we have both a ref and an object. We try to keep the ref, if the content is identical
-    const schema = { pointer: undefined, resolved: _.cloneDeep(_.omit(propValue, "$ref")) };
+    const schema = {
+      pointer: undefined,
+      resolved: _.cloneDeep(_.omit(propValue, "$ref")),
+    };
     const referenced = resolveRefNode(propValue, ctx, { deleteRef: false });
     if (_.isEqual(schema.resolved, referenced.resolved)) {
       cleanObj(propValue, []);
@@ -149,7 +197,11 @@ function findSchemaObjectsWithAllOf(bundled: OpenApiBundled) {
   return { collected, ctx: resolver };
 }
 
-function resolveRefNode(data: { $ref: string } | unknown, ctx: SchemaResolverContext, params?: { deleteRef: boolean }) {
+function resolveRefNode(
+  data: { $ref: string } | unknown,
+  ctx: SchemaResolverContext,
+  params?: { deleteRef: boolean }
+) {
   if (!isRef(data)) {
     return {
       pointer: undefined,
