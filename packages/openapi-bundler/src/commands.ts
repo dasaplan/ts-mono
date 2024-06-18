90b155b1ca8419d132c708dee618207ca80572a2
dsp <dev.dsp@posteo.de>
2024-06-18 23:21:30 +0200

    chore: minor fixes

--- packages/openapi-bundler/src/commands.ts
+++ packages/openapi-bundler/src/commands.ts
@@ -7,18 +7,45 @@ export function createCommandBundle(program: Command) {
   program
     .command("bundle")
     .description("Bundle Openapi specified files into a single file")
-    .argument("<openapi-spec>", "Absolut or Relative filepath from the cwd to the OpenApi root document file")
-    .option("-o, --outputFile [outputFile]", "Absolut or relative filepath to cwd of the resulting bundled file")
-    .option("--disableMergeAllOf", "If set, allOf arrays will be left as is after bundling")
-    .option("--disableEnsureDiscriminatorValues", "If set, discriminator values won't be ensured on every subType")
-    .action(async (spec: string, options: { outputFile?: string; disableMergeAllOf?: boolean; disableEnsureDiscriminatorValues?: boolean }) => {
-      const postProcessor = createSpecProcessor({
-        mergeAllOf: options.disableMergeAllOf ?? true,
-        ensureDiscriminatorValues: options.disableEnsureDiscriminatorValues ?? true,
-      });
-      const result = await withPerformance(() => bundleOpenapi(spec, { postProcessor, outFile: options.outputFile }));
-      log.info(`finished generate in ${(result.duration / 1000).toFixed(3)} s`, {outFile: result.ret.outFile});
-    });
+    .argument(
+      "<openapi-spec>",
+      "Absolut or Relative filepath from the cwd to the OpenApi root document file"
+    )
+    .option(
+      "-o, --outputFile [outputFile]",
+      "Absolut or relative filepath to cwd of the resulting bundled file"
+    )
+    .option(
+      "--disableMergeAllOf",
+      "If set, allOf arrays will be left as is after bundling"
+    )
+    .option(
+      "--disableEnsureDiscriminatorValues",
+      "If set, discriminator values won't be ensured on every subType"
+    )
+    .action(
+      async (
+        spec: string,
+        options: {
+          outputFile?: string;
+          disableMergeAllOf?: boolean;
+          disableEnsureDiscriminatorValues?: boolean;
+        }
+      ) => {
+        const postProcessor = createSpecProcessor({
+          mergeAllOf: options.disableMergeAllOf ?? true,
+          ensureDiscriminatorValues:
+            options.disableEnsureDiscriminatorValues ?? true,
+        });
+        const result = await withPerformance(() =>
+          bundleOpenapi(spec, { postProcessor, outFile: options.outputFile })
+        );
+        log.info(
+          `finished bundling in ${(result.duration / 1000).toFixed(3)} s`,
+          { outFile: result.ret.outFile }
+        );
+      }
+    );
 }
 
 async function withPerformance<T>(fun: () => Promise<T>) {
