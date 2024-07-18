b1c3d9abe52db3953a1877ce67bba8817716bc77
dsp <dev.dsp@posteo.de>
2024-07-18 18:09:58 +0200

    chore: improve error messages

--- packages/sdk/src/ts/error.ts
+++ packages/sdk/src/ts/error.ts
@@ -1,76 +1,91 @@
+import { _ } from "./lodash-extended.js";
+
 export class ApplicationError extends Error {
-    public static readonly NAME = "ApplicationError"
+  public static readonly NAME = "ApplicationError";
 
-    private constructor(message: string) {
-        super(message);
-        this.name = ApplicationError.NAME;
-    }
+  private constructor(message: string) {
+    super(message);
+    this.name = ApplicationError.NAME;
+  }
 
-    chainUnknown(error: unknown): ApplicationError {
-        const parsed = parseError(error);
-        switch (parsed.type){
-            case "ApplicationError":
-            case "ERROR":
-                return this.chain(parsed.error);
-            case "UNKNOWN_ERROR":
-                return this.chain(ApplicationError.from(parsed.error));
-        }
+  chainUnknown(error: unknown): ApplicationError {
+    const parsed = parseError(error);
+    switch (parsed.type) {
+      case "ApplicationError":
+      case "ERROR":
+        return this.chain(parsed.error);
+      case "UNKNOWN_ERROR":
+        return this.chain(ApplicationError.from(parsed.error));
     }
+  }
 
-    chain(error: Error | ApplicationError): ApplicationError {
-        return new ApplicationError(`${this.message}\n ::causedBy ${createOutMessageOfUnknownError(error)}`)
-    }
+  chain(error: Error | ApplicationError): ApplicationError {
+    return new ApplicationError(
+      `${this.message}\n ::causedBy ${createOutMessageOfUnknownError(error)}`
+    );
+  }
 
-    static create(message: string): ApplicationError {
-        return new ApplicationError(message)
-    }
+  static create(message: string): ApplicationError {
+    return new ApplicationError(message);
+  }
 
-    static from(error: unknown): ApplicationError {
-        const parsed = parseError(error);
-        switch (parsed.type){
-            case "ApplicationError":
-                return parsed.error
-            case "ERROR":
-                return ApplicationError.create(parsed.error.message)
-            case "UNKNOWN_ERROR":
-                return new ApplicationError(parsed.error)
-        }
+  static from(error: unknown): ApplicationError {
+    const parsed = parseError(error);
+    switch (parsed.type) {
+      case "ApplicationError":
+        return parsed.error;
+      case "ERROR":
+        return ApplicationError.create(parsed.error.message);
+      case "UNKNOWN_ERROR":
+        return new ApplicationError(parsed.error);
     }
+  }
 
-    public toString(): string {
-        return createOutMessage(this.name, this.message);
-    }
+  public toString(): string {
+    return createOutMessage(this.name, this.message);
+  }
 }
 
-function parseError(error: unknown):
-    {type: typeof ApplicationError.NAME, error: ApplicationError}
-     | {type: 'ERROR', error: Error}
-     | {type: 'UNKNOWN_ERROR', error: string}
-{
-    if (error instanceof ApplicationError) {
-        return {type: ApplicationError.NAME, error: error}
-    }
+function parseError(
+  error: unknown
+):
+  | { type: typeof ApplicationError.NAME; error: ApplicationError }
+  | { type: "ERROR"; error: Error }
+  | { type: "UNKNOWN_ERROR"; error: string } {
+  if (error instanceof ApplicationError) {
+    return { type: ApplicationError.NAME, error: error };
+  }
 
-    if (error instanceof Error) {
-        return {type: "ERROR", error: error}
-    }
+  if (error instanceof Error) {
+    return { type: "ERROR", error: error };
+  }
+  // instanceOf did not work for AssertionError - still not sure why
+  if (isLikelyError(error)) {
+    return { type: "ERROR", error: error };
+  }
 
-    return {type: "UNKNOWN_ERROR", error: JSON.stringify(error)}
+  return { type: "UNKNOWN_ERROR", error: JSON.stringify(error) };
 }
 
-function createOutMessageOfUnknownError(error: unknown) {
-    if (error instanceof ApplicationError) {
-        return error.toString()
-    }
+function isLikelyError(error: unknown): error is Error {
+  const cast = error as Error;
+  return (
+    _.isDefined(cast) &&
+    _.isDefined(cast.stack) &&
+    _.isDefined(cast.message) &&
+    typeof cast.stack === "string" &&
+    typeof cast.message === "string"
+  );
+}
 
-    if (error instanceof Error) {
-        return createOutMessage(error.name, error.message)
-    }
+function createOutMessageOfUnknownError(error: Error | ApplicationError) {
+  if (error instanceof ApplicationError) {
+    return error.toString();
+  }
 
-    return createOutMessage("UNKNOWN_ERROR", JSON.stringify(error))
+  return createOutMessage(error.name, error.message);
 }
 
 function createOutMessage(name: string, message: string) {
-    return `[${name}] ${message};`
+  return `[${name}] ${message};`;
 }
-
