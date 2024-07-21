58bd9cf978cc38989b3a6ac26133ee300f5e41c6
dsp <dev.dsp@posteo.de>
2024-07-21 19:35:38 +0200

    feat: suffix model names

--- packages/openapi/src/commands.ts
+++ packages/openapi/src/commands.ts
@@ -8,9 +8,9 @@ export function createCommandGenerate(program: Command) {
     .description("Generate Typescript and Zod files from an Openapi specified file")
     .argument("<openapi-spec>", "Relative filepath from the current cwd to the OpenApi root document file")
     .option("-o, --output [output]", "Target directory where the generated files will appear", "out")
-    .option("--suffix [suffix]", "Model suffix")
-    .action(async (spec: string, options: { output: string; suffix: string }) => {
-      await generateOpenapi(spec, options.output, { modelSuffix: options.suffix, clearTemp: true });
+    .option("--modelSuffix [modelSuffix]", "All model names are suffixed as provided")
+    .action(async (spec: string, options: { output: string; modelSuffix: string }) => {
+      await generateOpenapi(spec, options.output, { modelSuffix: options.modelSuffix, clearTemp: true });
       appLog.log.info(`finished generate`);
     });
 }
@@ -21,9 +21,9 @@ export function createCommandGenerateTs(program: Command) {
     .description("Generate typescript source code")
     .argument("<openapi-spec>", "Relative filepath from the current cwd to the OpenApi root document file")
     .option("-o, --output [output]", "Target directory for the generated files", "out")
-    .option("--suffix [suffix]", "Model suffix")
-    .action(async (spec: string, options: { output: string; suffix: string }) => {
-      await generateTsAxios(spec, options.output, { generateZod: false, modelSuffix: options.suffix });
+    .option("--modelSuffix [modelSuffix]", "All model names are suffixed as provided")
+    .action(async (spec: string, options: { output: string; modelSuffix: string }) => {
+      await generateTsAxios(spec, options.output, { generateZod: false, modelSuffix: options.modelSuffix });
       appLog.log.info(`finished generate`);
     });
 }
