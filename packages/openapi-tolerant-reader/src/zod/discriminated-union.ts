4ef3abf2a779e775a67f65408787e22434fa97ac
dsp <dev.dsp@posteo.de>
2025-04-27 11:29:43 +0200

    fix(codegen,zod): upgrade type

--- packages/openapi-tolerant-reader/src/zod/discriminated-union.ts
+++ packages/openapi-tolerant-reader/src/zod/discriminated-union.ts
@@ -2,13 +2,13 @@ import { z } from "zod";
 
 export module ZodDiscriminatedUnion {
   export type Handler<I> = z.Schema<I>;
-  export type MatchObj<T extends object, Discriminator extends keyof T> = Discriminator extends string
+  export type Matcher<T extends object, Discriminator extends keyof T> = Discriminator extends string
     ? {
         [K in T as K[Discriminator] extends string | number ? K[Discriminator] : never]: Handler<Extract<T, { type: K[Discriminator] }>>;
       } & { onDefault: Handler<{ [d in Discriminator]: string }> }
     : never;
 
-  export function of<T extends object>(discriminator: keyof T & string, matcher: ZodDiscriminatedUnion.MatchObj<T, typeof discriminator>): z.Schema<T> {
+  export function of<T extends object>(discriminator: keyof T & string, matcher: ZodDiscriminatedUnion.Matcher<T, typeof discriminator>): z.Schema<T> {
     return z
       .custom<T>()
       .transform((val) => {
@@ -26,16 +26,18 @@ export module ZodDiscriminatedUnion {
       .transform((v) => (v.result.success ? v.result.data : v.val));
   }
 
-  export function match<T extends object, D extends keyof T>(union: T, matcher: MatchObj<T, D>, discriminator: D): T {
+  export function match<T extends object, D extends keyof T>(union: T, matcher: Matcher<T, D>, discriminator: D): T {
     const handlerKey = union[discriminator] as keyof typeof matcher;
-    return handlerKey in matcher ? (matcher[handlerKey] as z.Schema).parse(union) : matcher.onDefault.parse(union);
+    return (handlerKey in matcher ? (matcher[handlerKey] as z.Schema<T>).parse(union) : matcher.onDefault.parse(union)) as T;
   }
   export function matchSafe<T extends object, D extends keyof T>(
     union: T,
-    matcher: MatchObj<T, D>,
-    discriminator: D
-  ): z.SafeParseSuccess<T> | z.SafeParseError<z.ZodError> {
+    matcher: Matcher<T, D>,
+    discriminator: D,
+  ): z.ZodSafeParseSuccess<T> | z.ZodSafeParseError<z.ZodError> {
     const handlerKey = union?.[discriminator] as keyof typeof matcher;
-    return handlerKey in matcher ? (matcher?.[handlerKey] as z.Schema)?.safeParse(union) : matcher.onDefault.safeParse(union);
+    return (handlerKey in matcher ? (matcher?.[handlerKey] as z.Schema<T>)?.safeParse(union) : matcher.onDefault.safeParse(union)) as
+      | z.ZodSafeParseSuccess<T>
+      | z.ZodSafeParseError<z.ZodError>;
   }
 }
