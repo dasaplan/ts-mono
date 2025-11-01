d59557cd682f6961c79f26e43a67a304cd40235d
dsp <dev.dsp@posteo.de>
2025-11-01 01:09:06 +0100

    feat(bundler): support endpoint filter by tag and operationId

---
+++ packages/openapi-tolerant-reader/eslint.config.cjs
@@ -0,0 +1,46 @@
+const {
+    defineConfig,
+} = require("eslint/config");
+
+const tsParser = require("@typescript-eslint/parser");
+const globals = require("globals");
+const js = require("@eslint/js");
+
+const {
+    FlatCompat,
+} = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+module.exports = defineConfig([{
+    languageOptions: {
+        parser: tsParser,
+
+        globals: {
+            ...globals.browser,
+        },
+
+        ecmaVersion: "latest",
+        sourceType: "module",
+
+        parserOptions: {
+            project: "./tsconfig.json",
+        },
+    },
+
+    extends: compat.extends(
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
+    ),
+
+    rules: {
+        "@typescript-eslint/switch-exhaustiveness-check": "error",
+        "no-inner-declarations": "off",
+        "@typescript-eslint/no-namespace": "off",
+    },
+}]);
